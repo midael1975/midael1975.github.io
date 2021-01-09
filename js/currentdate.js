87648c913cae23e24431382a283a6ca1e34dac7c
midael1975 <wjpd1905@gmail.com>
2021-01-09 15:20:17 -0400

    update

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,6 +1,10 @@
-  
-    const x = new Date();
-    d.setFullYear(2021);
-    document.getElementById(lastModified).innerHTML= x;
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-us', options);
 
+newFunction();
+function newFunction() {
+    var f = new Date();
+    document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+    
+}
 

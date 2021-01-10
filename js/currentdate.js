fd50468f1850575daa2509bbd1d2c96df008b5c5
midael1975 <wjpd1905@gmail.com>
2021-01-09 20:41:38 -0400

    update

--- js/currentdate.js
+++ js/currentdate.js
@@ -3,13 +3,3 @@ document.getElementById('currentdate').textContent = new Date().toLocaleDateS
 
 var d = new Date();
 document.getElementById("fullyear").innerHTML = d.getFullYear();
-
-// fileInput es un HTMLInputElement: <input type="file" multiple id="myfileinput">
-var fileInput = document.getElementById("myfileinput");
-
-// files es un objeto FileList (similar a NodeList)
-var files = fileInput.files;
-
-for (var i = 0; i < files.length; i++) {
-  alert(files[i].name + " tiene una fecha de ultima modificacion el " + files[i].lastModifiedDate);
-}

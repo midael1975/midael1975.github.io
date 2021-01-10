0f7741d8b2c51cf91223c388fb999ffbed92076c
midael1975 <wjpd1905@gmail.com>
2021-01-09 21:51:10 -0400

    update

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,4 +1,4 @@
-const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second:'numeric' };
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second:'numeric', timezone: 'long' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-us', options);
 
 var d = new Date();

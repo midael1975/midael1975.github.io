4b4c225c0399e87ed70facbb89b1d61b4ed7517d
midael1975 <wjpd1905@gmail.com>
2021-01-21 20:16:43 -0400

    update

---
+++ Lesson3/js/lesson3.js
@@ -0,0 +1,10 @@
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second:'numeric', timezone: 'long' };
+document.getElementById('design').textContent = new Date().toLocaleDateString('en-us', options);
+
+var d = new Date();
+document.getElementById("fullyear").innerHTML = d.getFullYear();
+
+function lastModified () {
+    document.write("Last Modified:" + document.lastModified);
+}
+ lastModified ();

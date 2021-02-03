4d1348c49ce9725eae535b38db6dc8490f8196de
midael1975 <wjpd1905@gmail.com>
2021-02-03 18:07:09 -0400

    init commit

--- Lesson 5/js/lesson5.js
+++ Lesson 5/js/lesson5.js
@@ -5,5 +5,20 @@ function toggleMenu () {
     document.getElementById('mostrar').classList.toggle('hide');
 }
 
+function day () {
+    let dayOfWeek = new Date().getDay();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let feedbackElement = document.getElementById("feedback");
+
+    let message;
+    if (dayOfWeek == 4) {
+        message =  "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
+    
+    }
+    feedbackElement.style.display = "block";
+    // add a class to move everything down so our message doesn't cover it.
+    document.body.classList.toggle("moveDown");
+    feedbackElement.innerHTML = message;
+}
 
 

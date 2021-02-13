cfc8cf0b456b7763940d33513e13024e1eda977b
midael1975 <wjpd1905@gmail.com>
2021-02-13 00:19:52 -0400

    init commit

--- Lesson 6/js/windChill.js
+++ Lesson 6/js/windChill.js
@@ -1,8 +1,11 @@
 function windChill(temp, speed){
-    
-    let chill = 35.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-    /*chill =(Math.round(chill)*100)/100;
-    console.log(chill);*/
+    let chill;
+    if ( temp <=50 && speed >3) {
+        chill = 35.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+
+    }
+    else {
+        chill = "N/A";  
+    }
     return chill.toFixed(2);
 }
-

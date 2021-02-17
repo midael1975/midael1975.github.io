d4d2eb67082620fcab8f929ea2d30deeb4b4ea97
midael1975 <wjpd1905@gmail.com>
2021-02-16 22:56:45 -0400

    init commit

---
+++ Lesson 7/js/lazy.js
@@ -0,0 +1,28 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 500px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+}
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver ((items, observer) =>{
+        items.forEach((item) => {
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+      });
+}
+
+  else {
+
+  } 

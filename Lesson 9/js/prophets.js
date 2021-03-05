7a1effd09536af11e776a78e624c43cc5d27b681
midael1975 <wjpd1905@gmail.com>
2021-03-05 01:00:25 -0400

    init commit

--- Lesson 9/js/prophets.js
+++ Lesson 9/js/prophets.js
@@ -14,9 +14,10 @@ let p = document.createElement('p');
 let image =document.createElement('img');
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + '. ' + '  ' + ' ' + 'Place of Birth:' + ' ' + prophets[i].birthplace;
+p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + '. ' + ' ' + 
+'Place of Birth:' + ' ' + prophets[i].birthplace;
 image.setAttribute('src', prophets[i].imageurl);
-image.setAttribute( 'alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + prophets[i].order)
+image.setAttribute( 'alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + prophets[i].order);
 
 card.appendChild(h2);
 card.appendChild(p);

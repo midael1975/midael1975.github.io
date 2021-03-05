b3a643b936b07257756ad5bc249eb8fba0d883f3
midael1975 <wjpd1905@gmail.com>
2021-03-05 18:18:32 -0400

    init commit

--- Lesson 9/js/home.js
+++ Lesson 9/js/home.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = 'https://lesson 9/home.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -9,18 +9,18 @@ fetch(requestURL)
     const towns = jsonObject['towns'];
     for (let i = 0; i < towns.length; i++ ) {
 let home = document.createElement('section');
-let h1 = document.createElement('h1');
-let h2 = document.createElement('h2')
+let h2 = document.createElement('h2');
+let h3 = document.createElement('h3')
 let p = document.createElement('p');
 let image =document.createElement('img');
 
-h1.textContent = towns[i].name;
-h2.textContent = towns[i].motto;
+h2.textContent = towns[i].name;
+h3.textContent = towns[i].motto;
 p.textContent = 'Year founded:' + ' ' + towns[i].yearFounded + ' ' + 'Population:' + ' ' + towns[i].currentPopulation + ' ' + 'Annual Rain Fall:' + ' ' + towns[i].averageRainfall;
 image.setAttribute('src', towns[i].photo);
 
-home.appendChild(h1);
 home.appendChild(h2);
+home.appendChild(h3);
 home.appendChild(p);
 home.appendChild(image);
 

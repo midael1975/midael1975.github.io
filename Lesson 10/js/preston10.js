7a678bfdec6ba73b6459fadbdabe06c65607df37
midael1975 <wjpd1905@gmail.com>
2021-03-10 20:42:41 -0400

    init commit

--- Lesson 10/js/preston10.js
+++ Lesson 10/js/preston10.js
@@ -13,7 +13,7 @@ fetch(apiURL)
     document.getElementById('td3').textContent = jsObject.main.temp;
     document.getElementById('td4').textContent = jsObject.main.temp;
     
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[4].icon + '.png';  // note the concatenation
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
 document.getElementById('imagesrc').getAttribute = imagesrc;// informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method

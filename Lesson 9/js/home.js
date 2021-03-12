bf3164d725d8ac87b63f3ebdd0f2b29c3f8a6604
midael1975 <wjpd1905@gmail.com>
2021-03-12 02:24:15 -0400

    init commit

--- Lesson 9/js/home.js
+++ Lesson 9/js/home.js
@@ -15,13 +15,15 @@ let res = document.createElement('section');
 let h2 = document.createElement('h2');
 let h4 = document.createElement('h4')
 let p = document.createElement('p');
+let people = document.createElement('p');
+let annual =document.createElement('p');
 let image =document.createElement('img');
 
 h2.textContent = towns[i].name;
 h4.textContent = towns[i].motto;
-p.textContent = 'Year founded:' + ' ' + towns[i].yearFounded + ' ' + 
-'Population:' + ' ' + towns[i].currentPopulation + ' ' + 
-  'Annual Rain Fall:' + ' ' + towns[i].averageRainfall;
+p.textContent = 'Year founded:' + ' ' + towns[i].yearFounded;
+people.textContent = 'Population:' + ' ' + towns[i].currentPopulation;
+annual.textContent = 'Annual Rain Fall:' + ' ' + towns[i].averageRainfall;
 image.setAttribute('src', `images/${towns[i].photo}`);
 image.setAttribute( 'alt', towns[i].name);
 image.setAttribute('class', "left");
@@ -29,6 +31,8 @@ image.setAttribute('class', "left");
 home.appendChild(h2);
 home.appendChild(h4);
 home.appendChild(p);
+home.appendChild(people);
+home.appendChild(annual);
 home.appendChild(image);
 res.appendChild(home);
         res.appendChild(image);

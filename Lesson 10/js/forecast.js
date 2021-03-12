a4b477633d81ab60bbb41c18b64a01594feac739
midael1975 <wjpd1905@gmail.com>
2021-03-12 03:15:38 -0400

    init commit

--- Lesson 10/js/forecast.js
+++ Lesson 10/js/forecast.js
@@ -1,55 +1,53 @@
-function getForecast(){
-    const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=7d622b58ff7b6823c7020ccafbb6f9d1';
-    fetch(apiURL)
-      .then((response) => response.json())
-      .then((jsObject) => {
-        
-        const preston = jsObject; //paso el objeto a una variable para mas facilidad
-        let temp;//declaro la temperatura
-        let fecha;// declaro la fecha
-        // declaro dia todo vacios para usarlo dentro del ciclo forb
-        
-        let c =1;//para contar solo interaciones positivas y usarlas para formar el "id" de lo elementos
-        for(let i = 0; i <= preston.list.length-1; i++){// recorrer el array json solo la lista
-          let hora = new Date(preston.list[i].dt_txt).getHours();// obtener la hora de cada elemento de la lista
-          
-          if(hora==18){// si es igual a los que nos piden
-            temp = preston.list[i].main.temp;// obtengo el pronostico
-            fecha = preston.list[i].dt_txt;// obtengo la fecha para luego obtener el dia
-            day = dayName(fecha);// funcion creada para obtener el dia de la semana
-            let dia = "0" + c;//concademacion para formar los id del div que dice el dia de la semana
-            let day = "day" +c;// para el span que esta dentro del div que muestra la temperatura
-            let icon = "icon" + c;// para el id de la img que muestra el icono
-            document.getElementById(dia).textContent = day;
-            document.getElementById(day).textContent = temp;
-            const imagesrc = 'https://openweathermap.org/img/w/' + preston.list[i].weather[0].icon + '.png'; 
-            const desc = preston.list[i].weather[0].description;
-            document.getElementById(icon).setAttribute('src', imagesrc);
-            document.getElementById(icon).setAttribute('alt', desc);
-            c++;
+function getForescast() {
+const apiforecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=7d622b58ff7b6823c7020ccafbb6f9d1';
+fetch(apiforecastURL)
+.then((response) => response.json())
+.then((jsonObject) => {
+  const preston = jsonObject['preston'];
+  let temp;
+  let fecha;
+  let day;
+  let x =1;
+  for (let i = 0; i < preston.list.length-1; i++ ) {
+    if (preston[i].dt_tx == '18.00.00'){
+temp = preston.list[i].main.temp;
+fecha = preston.list[i].dt_tx;
+day = dayName(fecha);
+let dia = "0" + x;
+let day = "day" + x;
+let icon = "icon" + x;
 
+document.getElementById(day).textContent = temp;
 
-          }
-          
+const imagesrc = 'https://openweathermap.org/img/w/' + preston.list[i].weather[0].icon + '.png';
+const desc = preston.list[i].weather[0].description;
 
-        }       
+document.getElementById(icon).setAttribute('src', imagesrc);
+document.getElementById(icon).setAttribute('alt', desc);
+x++;
 
-      });
     }
-  function dayName(fecha){
+  }
+});
 
-    const days = [
-    'Sun.',
-    'Mon.',
-    'Tue.',
-    'Wen.',
-    'Thu.',
-    'Fri.',
-    'Sat.',
-    
-    ];
-    const numberDay = new Date(fecha).getDay();//recordar que javascript solo devule el dia en numero,
-    const nameDay = days[numberDay];           //comienza el domingo = 0, asi consigo el dia en letras
-
-    return nameDay;
 }
+
+
+function dayName(fecha){
+
+  const days = [
+  'Sun.',
+  'Mon.',
+  'Tue.',
+  'Wen.',
+  'Thu.',
+  'Fri.',
+  'Sat.',
+  
+  ];
+  const numberDay = new Date(fecha).getDay();
+  const nameDay = days[numberDay];
+
+}
+
+

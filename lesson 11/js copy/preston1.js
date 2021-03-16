ea99d700c73a99fab5a358b73df16cbb7a81ed34
midael1975 <wjpd1905@gmail.com>
2021-03-16 12:21:23 -0400

    init commit

---
+++ lesson 11/js copy/preston1.js
@@ -0,0 +1,26 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=7d622b58ff7b6823c7020ccafbb6f9d1';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('description').textContent = jsObject.weather[0].main;
+    document.getElementById('temp').textContent = jsObject.main.temp;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('speed').textContent = jsObject.wind.speed;
+
+   let t = parseFloat(document.getElementById("temp").innerHTML);
+    let s = parseFloat(document.getElementById("speed").innerHTML);
+     
+    if (t <= 50 && s > 3) {
+      let chill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    
+      chill = document.getElementById("windchill").innerHTML =
+        chill.toFixed(2);
+    } 
+    
+    else {
+      chill = "Not Applicable";
+    
+    } 
+
+});

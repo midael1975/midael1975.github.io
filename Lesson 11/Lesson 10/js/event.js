bd92b5add3d537d02e07541880acd137d649e8f9
midael1975 <wjpd1905@gmail.com>
2021-03-20 22:58:49 -0400

    init commit

---
+++ Lesson 11/Lesson 10/js/event.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    let x =0;
+    for (let i = 0; i < towns.length; i++ ) {
+      if (towns[i].name == "Preston"){
+let home = document.createElement('section');
+let title = document.createElement('p');
+
+
+
+title.textContent = towns[i].events;
+
+
+home.appendChild(title);
+
+
+
+x++;
+document.querySelector('div.events').appendChild(home);
+      }
+    }
+  });
+ 
+
+

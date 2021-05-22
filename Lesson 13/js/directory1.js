41d70869f4e1a3c135f75d563797c4c682b72815
midael1975 <wjpd1905@gmail.com>
2021-05-22 11:38:09 -0400

    init commit

--- Lesson 13/js/directory1.js
+++ Lesson 13/js/directory1.js
@@ -1,31 +1,30 @@
 const URL = 'json/directory.json';
+
 function come() {
-fetch(URL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const business = jsonObject['business'];
-    for (let i = 0; i < business.length; i++ ) {
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
-let p = document.createElement('p');
-let image =document.createElement('img');
+    fetch(URL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonObject) {
+            console.table(jsonObject); // temporary checking for valid response and data parsing
+            const business = jsonObject['business'];
+            for (let i = 0; i < business.length; i++) {
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let p = document.createElement('p');
+                let image = document.createElement('img');
 
-h2.textContent = business[i].name;
-p.textContent =  'Contact Information:' + ' ' + business[i].contactinfo;
-image.setAttribute('src', business[i].logo);
-image.setAttribute( 'alt', business[i].name);
+                h2.textContent = business[i].name;
+                p.textContent = 'Contact Information:' + ' ' + business[i].contactinfo;
+                image.setAttribute('src', business[i].logo);
+                image.setAttribute('alt', business[i].name);
 
-card.appendChild(h2);
-card.appendChild(p);
-card.appendChild(image);
+                card.appendChild(h2);
+                card.appendChild(p);
+                card.appendChild(image);
 
-document.querySelector('div.list').appendChild(card);
+                document.querySelector('div.list').appendChild(card);
+            }
+        });
+
 }
-  })
-  
-}
-
-
